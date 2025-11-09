113bfcde0610b9c76ab6d954400d17b7462e6a67
jpb05277 <ja240277@ucf.edu>
2025-11-09 14:10:06 -0500

    Add RSVP and cancel RSVP routes

--- backend/src/routes/eventRoutes.js
+++ backend/src/routes/eventRoutes.js
@@ -6,7 +6,12 @@ const {
     createEvent
 } = require("../controllers/eventControllers");
 
+import { rsvpEvent, cancelRsvp } from "../controllers/eventController.js";
+import { protect } from "../middleware/auth.js";
+
 router.get("/", getAllEvents);
 router.post("/", createEvent);
+router.post("/:id/rsvp", protect, rsvpEvent);
+router.post("/:id/cancel-rsvp", protect, cancelRsvp);
 
 module.exports = router;
