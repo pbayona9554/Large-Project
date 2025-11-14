fad4afc520b4e804bf8a1e7e2437f474629d35d0
Paola Bayona <bayonapaola2002@gmail.com>
2025-11-14 17:14:42 -0500

    added a server.proxy section to vite config to allow for local testing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000", // your backend when testing locally
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
